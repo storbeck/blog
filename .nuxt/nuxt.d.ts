443b076afd09e3cf393fbb68229f4ba20e17b879
Geoff Storbeck <geoff@pagefoundry.dev>
2026-02-12 22:10:06 -0500

    nit styling

--- .nuxt/nuxt.d.ts
+++ .nuxt/nuxt.d.ts
@@ -1,5 +1,5 @@
-/// <reference types="@nuxt/telemetry" />
 /// <reference types="@nuxt/content" />
+/// <reference types="@nuxt/telemetry" />
 /// <reference types="@nuxt/devtools" />
 /// <reference path="types/builder-env.d.ts" />
 /// <reference path="types/plugins.d.ts" />
